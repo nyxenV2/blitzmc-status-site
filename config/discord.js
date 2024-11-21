4f2e1da8240088202b41d84ca8864137012f1c61
nyxenV2 <168292627+nyxenV2@users.noreply.github.com>
2024-11-21 08:12:04 +0100

    Update discord.js

--- config/discord.js
+++ config/discord.js
@@ -1,5 +1,5 @@
 // config/discord.js
 
 module.exports = {
-    webhookUrl: 'https://discord.com/api/webhooks/1309050142204100608/MWvGYr02KEdt3WSwD8bq7gQ09-l3qIGDDCZl-GYW0U6pa0Y2sE2tgjRu576c22mcCOL9'
+    webhookUrl: ''
 };
